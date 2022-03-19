cb74450b0c898c1113edf9dbadd5b6d708565832
JasonLuis <jason20luis@gmail.com>
2022-03-19 15:52:39 -0300

    chore: first commit

---
+++ conceitos/lifecycle-hocks/src/App.vue
@@ -0,0 +1,24 @@
+/* Documentação dos life-cycle hooks: https://v3.vuejs.org/guide/composition-api-lifecycle-hooks.html#lifecycle-hooks */ 
+<template>
+  <h1>Lifecycle hooks</h1>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+export default {
+  setup () {
+    onMounted(() => {
+      console.log('Foi montado composition API')
+    })
+  }
+}
+/* export default {
+  mounted () {
+    console.log('Foi montado options API');
+  }
+} */
+</script>
+
+<style>
+
+</style>
